c15b463a5df3fd2f88a40c2e2bb2067d08b742b0
Prasad Ambaledath <prasad.ambaledath@convergint.com>
2025-01-18 18:31:37 +0530

    poka

--- src/pages/page1/page1.tsx
+++ src/pages/page1/page1.tsx
@@ -1,26 +1,18 @@
 import { useCallback, useState } from "react";
-import Toolbar, { Item } from "devextreme-react/toolbar";
 import Switch, { SwitchTypes } from "devextreme-react/switch";
 import "./page1.scss";
 
 export default function Page1() {
-  const [showIncomplete, setShowIncomplete] = useState(false);
+  const [value, setValue] = useState(false);
 
-  const showIncompleteValueChange = useCallback(
-    (e: SwitchTypes.ValueChangedEvent) => {
-      setShowIncomplete(e.value);
-    },
-    []
-  );
+  const valueChanged = useCallback((e: SwitchTypes.ValueChangedEvent) => {
+    setValue(e.value);
+  }, []);
 
   return (
     <div className="page1">
-      <Switch
-        value={showIncomplete}
-        onValueChanged={showIncompleteValueChange}
-      />
-
       <div>page 1</div>
+      <Switch value={value} onValueChanged={valueChanged} />
     </div>
   );
 }
