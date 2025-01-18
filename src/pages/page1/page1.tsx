1798a4c16ebb2ced15725160140d541221caa7ca
Prasad Ambaledath <prasad.ambaledath@convergint.com>
2025-01-18 18:42:56 +0530

    react-switch

--- src/pages/page1/page1.tsx
+++ src/pages/page1/page1.tsx
@@ -1,18 +1,33 @@
 import { useCallback, useState } from "react";
-import Switch, { SwitchTypes } from "devextreme-react/switch";
+//import Switch, { SwitchTypes } from "devextreme-react/switch";
+import Switch from "react-switch";
 import "./page1.scss";
 
 export default function Page1() {
-  const [value, setValue] = useState(false);
+  const [checked, setChecked] = useState(false);
 
-  const valueChanged = useCallback((e: SwitchTypes.ValueChangedEvent) => {
-    setValue(e.value);
-  }, []);
-
+  const valueChanged = () => {
+    setChecked(!checked);
+  };
+  console.log(checked);
   return (
     <div className="page1">
       <div>page 1</div>
-      <Switch value={value} onValueChanged={valueChanged} />
+      <Switch
+        onChange={valueChanged}
+        checked={checked}
+        onColor="#86d3ff"
+        onHandleColor="#2693e6"
+        handleDiameter={30}
+        uncheckedIcon={false}
+        checkedIcon={false}
+        boxShadow="0px 1px 5px rgba(0, 0, 0, 0.6)"
+        activeBoxShadow="0px 0px 1px 10px rgba(0, 0, 0, 0.2)"
+        height={20}
+        width={48}
+        className="react-switch"
+        id="material-switch"
+      />
     </div>
   );
 }
