c12d5f5deff5902c43223bf2cb82c461edf98b12
Prasad Ambaledath <prasad.ambaledath@convergint.com>
2025-01-19 15:18:40 +0530

    TabPanel

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,59 +1,22 @@
-import { useCallback, useEffect, useState } from "react";
-import Tabs from "devextreme-react/tabs";
-import { useNavigate, useLocation } from "react-router-dom";
-import { SelectionChangedEvent } from "devextreme/ui/tabs";
-import "./Header.scss";
+import TabPanel, { Item } from "devextreme-react/tab-panel";
+import { Page1, Page2, Page3 } from "../../pages";
 
-const tabsIconAndText = [
-  {
-    id: 0,
-    text: "Page 1",
-    path: "/page1",
-  },
-  {
-    id: 1,
-    text: "Page 2",
-    path: "/page2",
-  },
-  {
-    id: 2,
-    text: "Page 3",
-    path: "/page3",
-  },
-];
-
-export default function Header() {
-  const navigate = useNavigate();
-  const location = useLocation();
-  console.log("location.pathname : ", location.pathname);
-  const [selectedTab, setSelectedTab] = useState(tabsIconAndText[0]);
-  const [tabIndex, setTabIndex] = useState(tabsIconAndText[0].id);
-
-  const onNavigationChanged = useCallback((args) => {
-    const tabObj = args.selectedItem || args.addedItems[0];
-    setSelectedTab(args.selectedItem || args.addedItems[0]);
-    setTabIndex(tabObj.id);
-    localStorage.setItem("path", tabObj.path);
-    localStorage.setItem("tabindex", tabObj.id);
-    navigate(tabObj.path);
-  }, []);
-
-  useEffect(() => {
-    const path = localStorage.getItem("path");
-    const tabindex = localStorage.getItem("tabindex");
-    console.log("tabindex : ", tabindex);
-    console.log("path : ", path);
-    navigate(path);
-  }, [navigate, selectedTab]);
-
+const Header = () => {
   return (
     <header className={"header-component"}>
-      <Tabs
-        dataSource={tabsIconAndText}
-        stylingMode={"secondary"}
-        onSelectionChanged={onNavigationChanged}
-        selectedIndex={1}
-      />
+      <TabPanel animationEnabled={false}>
+        <Item title="Employee" icon="floppy">
+          <Page1 />
+        </Item>
+        <Item title="Notes" icon="comment">
+          <Page2 />
+        </Item>
+        <Item title="Role" icon="isnotblank">
+          <Page3 />
+        </Item>
+      </TabPanel>
     </header>
   );
-}
+};
+
+export default Header;
